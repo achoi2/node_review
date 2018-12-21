f3e6e762cf41e283131f38ee8f3b37e5a5295166
Andrew <andrewchoi1@hotmail.com>
2018-12-20 23:10:55 -0500

    added argument to message

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const EventEmitter = require('events');
 const emitter = new EventEmitter();
+const logger = new EventEmitter();
 
 //Register a listener
-emitter.on('messageLogged', function() {
-    console.log('Listener called');
+emitter.on('messageLogged', (arg) => {
+    console.log('Listener called', arg);
 });
 
 // raise an event
-emitter.emit('messaged logged');
+emitter.emit('messaged logged', { id: 1, url: 'http//' });
+
+logger.on('logged', (arg) => {
+    console.log('logged', arg)
+});
+
+logger.emit('logged', { data: 'message'})
