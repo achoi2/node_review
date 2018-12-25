db47b82d4a362341a55ae5aeaf1458d773cd7c8b
Andrew <andrewchoi1@hotmail.com>
2018-12-24 21:17:15 -0500

    added a get request

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    { id: 1, name: 'course1'},
+    { id: 2, name: 'course2' },
+    { id: 3, name: 'course3' }
+];
+
 app.get('/', (req, res) => {
     res.send('hello world!!')
 });
 
 app.get('/api/courses', (req, res) => {
-    res.send([1,2,3]);
+    res.send(courses);
 });
 
 app.get('/api/courses/:id', (req, res) => {
-    res.send(req.params.id)
+    const course = courses.find(course => course.id === parseInt(req.params.id));
+    if(!course) res.status(404).send('not found')
 });
 
 const port = process.env.PORT || 3000;
