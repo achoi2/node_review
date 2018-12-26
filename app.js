a8b6c7ae07b08898ce14ef900a4c475bf7961b86
Andrew <andrewchoi1@hotmail.com>
2018-12-25 22:39:06 -0500

    added post request

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const courses = [
     { id: 1, name: 'course1'},
     { id: 2, name: 'course2' },
@@ -18,7 +20,20 @@ app.get('/api/courses', (req, res) => {
 app.get('/api/courses/:id', (req, res) => {
     const course = courses.find(course => course.id === parseInt(req.params.id));
     if(!course) res.status(404).send('not found')
+    res.send(course)
 });
+
+app.post('/api/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
+});
+
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
