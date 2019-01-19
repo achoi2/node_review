0600d27b13520a5e8d7f40e5baaf5c273d720f88
Andrew <andrewchoi1@hotmail.com>
2019-01-19 17:42:32 -0500

    added lat and long

--- weather/app.js
+++ weather/app.js
@@ -7,6 +7,8 @@ request(
         json: true
     },
     (error, response, body) => {
-        console.log(JSON.stringify(body, undefined, 2));
+        console.log(`Address: ${body.results[0].providedLocation.location}`);
+        console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+        console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
     }
 );
