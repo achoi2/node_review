0190561a3677e08d5e2dd3ae25d5577c40808dec
Andrew <andrewchoi1@hotmail.com>
2019-01-18 21:45:29 -0500

    made json object readable

--- weather/app.js
+++ weather/app.js
@@ -1,8 +1,12 @@
 const request = require('request');
 
-request({
-    url: 'http://',
-    json: true
-}, (error, response, body) => {
-    console.log(body)
-});
+request(
+    {
+        url:
+            'http://www.mapquestapi.com/geocoding/v1/address?key=aaD1WP7OACu69YrPCnK1ATZvbMLOCMkt&location=1301%20lombard%20street%20philadelphia',
+        json: true
+    },
+    (error, response, body) => {
+        console.log(JSON.stringify(body, undefined, 2));
+    }
+);
