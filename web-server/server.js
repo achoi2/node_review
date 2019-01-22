02a4d958f539c68eb66585fe86499f34678af43f
Andrew <andrewchoi1@hotmail.com>
2019-01-21 21:30:00 -0500

    served up static html files

--- web-server/server.js
+++ web-server/server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 let app = express();
 
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', (req, res) => {
     // res.send('hello express')
     res.send({
@@ -23,4 +25,6 @@ app.get('/bad', (req, res) => {
     });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('server is up on port 3000')
+});
