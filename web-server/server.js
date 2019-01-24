b4b800806f37fa26128c5df83e359a240c59a7be
Andrew <andrewchoi1@hotmail.com>
2019-01-24 17:44:17 -0500

    added express middleware

--- web-server/server.js
+++ web-server/server.js
@@ -1,10 +1,29 @@
 const express = require('express');
-const hbs = require('hbs')
+const hbs = require('hbs');
+const fs = require('fs');
 
 let app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now}: ${req.method} ${req.url}`;
+    
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('error')
+        }
+    });
+    next();
+}); 
+
+// app.use((req, res, next) => {
+//     res.render('maintenance.hbs')
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -14,6 +33,8 @@ hbs.registerHelper('getCurrentYear', () => {
 hbs.registerHelper('capitalizeIt', (text) => {
     return text.toUpperCase();
 });
+
+
 
 app.get('/', (req, res) => {
     // res.send('hello express')
